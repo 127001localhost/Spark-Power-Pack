7512e6d8a2a9b0417d6acbdf7a382fae262fe9b4
Brad McAllister <bdm@bdmcomputers.com>
2016-03-11 00:12:17 -0600

    created exodus

--- js/delete.js
+++ js/delete.js
@@ -1,4 +1,5 @@
 var myRooms = [];
+var selected = [];
 var sparkToken = localStorage.getItem("sparkToken");
 
 console.log(sparkToken);
@@ -6,6 +7,46 @@ console.log(sparkToken);
 //////////////////////////////////////
 // Site Layout Control
 /////////////////////////////////////
+
+function leaveSelected(){
+	console.log(selected.length);
+	for(i in selected){
+		console.log("this would leave: "+myRooms[selected[i]].title);
+		getMembershipId(myRooms[selected[i]].id, localStorage.getItem("myId"));
+
+	}
+}
+
+function getMembershipId(roomId, personId){
+	$.ajax({
+		url: "https://api.ciscospark.com/v1/memberships?roomId="+roomId+"&personId="+personId,
+		headers: {'Content-Type': 'application/json', 'Authorization': sparkToken},
+		cache: false,
+		method: "GET"
+	}).done(function(data){
+		console.log(data);
+		leaveRoom(data.items[0].id);
+	});
+}
+
+function leaveRoom(membershipId){
+	var count = 0;
+	$.ajax({
+		url: "https://api.ciscospark.com/v1/memberships/"+membershipId,
+		headers: {'Content-Type': 'application/json', 'Authorization': sparkToken},
+		cache: false,
+		method: "DELETE",
+		statusCode: {
+			204: function(){
+				count++;
+			}
+		}
+	});
+	$("#confirm").empty();
+	$("#complete").show();
+	$("#complete").html("<h3>Success!</h3>");
+
+}
 
 
 function roomsClick(){
@@ -17,7 +58,7 @@ function roomsClick(){
 			if (xhttp.status == 200) {
 				var rooms = JSON.parse(xhttp.responseText);
 				for(var i = 0; i < rooms['items'].length; i++){
-					console.log(rooms['items'][i].title);
+					//console.log(rooms['items'][i].title);
 					myRooms.push(rooms['items'][i]);
 					var roomName = rooms['items'][i].title;
 					var roomId = rooms['items'][i].id;
@@ -25,7 +66,7 @@ function roomsClick(){
 				}
 				HTML += "</table>";
 				$("#myRooms").show();
-				$("#myRooms").html(HTML);
+				$("#myRooms").prepend(HTML);
 				$("#intro").hide();
 			}else{
 				console.log('Error: ' + xhttp.statusText);
@@ -44,14 +85,21 @@ function refreshToken(){
 }
 
 function reviewSelected(){
-	var selected = [];
 	$("#roomList :checked").each(function(){
 		selected.push($(this).val());
-		console.log(myRooms[$(this).val()].id);
 	});
+	$("#myRooms").hide();
+	$("#confirm").show();
+	var HTML = "<h2>Are you sure you wish to leave the following rooms?</h2>";
+	for(index in selected){
+		HTML += "<p>"+myRooms[selected[index]].title+"</p>";
+	};
 	console.log(selected);
+	$("#confirm").prepend(HTML);
 }
 
 function startOver(){
+	selected = [];
+	myRooms = [];
 	location.reload();
 }

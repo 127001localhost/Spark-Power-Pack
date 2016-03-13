42ba02565d579ed9056a4e1ee653d39b09437367
Brad McAllister <bdm@bdmcomputers.com>
2016-03-13 14:24:33 -0500

    JavaScript Cleanup
    - moved global functions and variables from delete.js to common.js
    - ordered functions alphabetically, commented out all console.log
    statements

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+//Global variables
+var sparkToken = localStorage.getItem("sparkToken");
 
 $("#profile").append("<img class=\"user dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\" src=\""+localStorage.getItem("myAvatar")+"\">");
 

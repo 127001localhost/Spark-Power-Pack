71f1bd12da6a3fbc575f3ab91aeabfbd4b57413c
Brad McAllister <bdm@bdmcomputers.com>
2016-03-18 15:39:22 -0500

    token check
    added a check in the common.js to look for the SparkToken in
    localStorage. If it isn’t found the user is redirected to index.html to
    authorize the app.

--- js/common.js
+++ js/common.js
@@ -1,9 +1,13 @@
 //Global variables
-var sparkToken = localStorage.getItem("sparkToken");
+if(localStorage.getItem("sparkToken")){
+	var sparkToken = localStorage.getItem("sparkToken");
 
-$("#profile").append("<img class=\"user dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\" src=\""+localStorage.getItem("myAvatar")+"\">");
+	$("#profile").append("<img class=\"user dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\" src=\""+localStorage.getItem("myAvatar")+"\">");
 
-function refreshToken(){
-	localStorage.removeItem("sparkToken");
+	function refreshToken(){
+		localStorage.clear();
+		window.location="index.html";
+	}
+}else{
 	window.location="index.html";
 }

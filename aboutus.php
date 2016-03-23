507cdc6eceea5c3d50b5fcabdd891a750dde37ba
Brad McAllister <bdm@bdmcomputers.com>
2016-03-23 16:28:10 -0500

    added about us page

---
+++ aboutus.php
@@ -0,0 +1,37 @@
+ <?php
+  include 'includes/header.php';
+?>
+  <?= $header; ?>
+	<div class="container">
+	<div class="row">
+		<div class="col-md-4"><div style="text-align: center;"><h3>Ang Tan</h3><h4>WI-IL Territory - SE</h4><img class="profile-large" src="https://1efa7a94ed216783e352-c62266528714497a17239ececf39e9e2.ssl.cf1.rackcdn.com/V1~913794648422a7a613b164f2b21ec5f5~cyOT4UDOSPiV8Xk7_0akzg==~1600" height="100px">
+		<h4>Software Developer</h4><a href="https://www.linkedin.com/in/antan"><img src="images/linkedin.png" width="40px"></a><a href="tel:+14088943509"><img src="images/phone.png" width="35px"></a></div></div>
+		
+		<div class="col-md-4"><div style="text-align: center;"><h3>Brad McAllister</h3><h4>Collaboration - CSE</h4><img class="profile-large" src="https://1efa7a94ed216783e352-c62266528714497a17239ececf39e9e2.ssl.cf1.rackcdn.com/V1~10d413100dd1b83b9dc0dc1a36ad81c8~ej_yZ9Y4Rb--e8CTc9yb-A==~1600" height="100px">
+		<h4>Software Developer</h4><a href="https://www.linkedin.com/in/bmcallis"><img src="images/linkedin.png" width="40px"></a><a href="http://www.twitter.com/bmcallister"><img src="images/twitter.png" width="40px"></a><a href="tel:+18476786776"><img src="images/phone.png" width="35px"></a></div></div>
+
+		<div class="col-md-4"><div style="text-align: center;"><h3>Jim Matthews</h3><h4>CH-IN Territory - SE</h4><img class="profile-large" src="https://1efa7a94ed216783e352-c62266528714497a17239ececf39e9e2.ssl.cf1.rackcdn.com/V1~e27a398d57b03c0c7b3c0b2568161b06~Z42bkfnyTC2S_xwnKsp3_Q==~1600" height="100px">
+		<h4>Director of User Experience</h4><img src="images/linkedin.png" width="40px"></a><a href="http://www.twitter.com/bmcallister"><img src="images/twitter.png" width="40px"></a><a href="tel:+18476786776"><img src="images/phone.png" width="35px"></a></div></div>
+
+	</div>
+		<div class="row">
+		<div class="jumbotron">
+			<p>The Spark Power Pack application(s) each leverage the powerful API's available to Spark Developers. The initial set of API's focus on the management of Spark Rooms.  With access to Spark Power Pack, you can manage your rooms and the people in them. Other features include important room notification via Spark to Cisco Jabber.</p>
+			<ul>
+				<li>Need to update multiple team rooms at the same time with an important message or an alert? Try Carbon Copy.</li>
+				<li>Need to create a room for a special event? Invites can be used to quickly create and add 100s of users to a room. </li>
+				<li>Been using Spark for a while? Use Exodus to remove yourself from rooms in bulk.</li>
+				<li>Get the best of Jabber & Spark with Tab Manager. Setup alerts with Tab Manager to send a notification to Jabber when an important conversation in Cisco Spark is happening.</li>
+			</ul>
+		</div>
+
+	</div>
+
+	</div>
+</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/common.js"></script>
+</html>
+
+

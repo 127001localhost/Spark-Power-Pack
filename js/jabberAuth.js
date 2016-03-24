a39b77332b04d253c6d461464c4425fbd55e38d8
Brad McAllister <bdm@bdmcomputers.com>
2016-03-23 20:01:04 -0500

    updated to use server side auth.

--- js/jabberAuth.js
+++ js/jabberAuth.js
@@ -1,23 +1,22 @@
-//Sample application demonstrating how to perform a Spark OAuth2 authentication and Spark API access token request
-//Note: this page must be accessed from a web server, Step #3 below may fail if loaded from the file system
-// Helper function that generates a random alpha/numeric string
 var sparkToken = "";
 var clientId = "Cfad29d7cc842ef10a7679ca8e42826edd16abd241c46c9b43069087fd5d32e4f";
-var clientSecret = "3ece20091f18e64dc9adbc8a3b30e4d0c0bebce1e8e53a902a0e1bf68881122a";
-var appRedirectUri="http://me.bmcallister.com/spark/jabberTab.php";
+var appRedirectUri="http://spark.bdmcomputers.com/jabberTab.php";
 var accessCode="";
 
 // If so user has already authenticated, and  page has been reloaded via the Redirect URI
 window.onload = function(e) {
-	var params = parseQueryStr(window.location.search.substring(1)); // Parse the query string params into a dictionary
-	if (params['code']) { // If the query param 'code' exists, then...
-		accessCode = params['code'];
-		requestToken();
-	}else if (params['error']) { // If the query param 'error' exists, then something went wrong...
-		alert('Error requesting auth code: ' + params['error'] + ' / ' + params['error_description']);
-	}else if (localStorage.getItem("sparkToken") == null) {
-		requestCode();
+	if (localStorage.getItem("sparkToken") != null) {
+		window.location="powerpack.php";
 	}else{
+		//document.getElementById('redirectUri').value=window.location.href.split("?")[0]; // Detect the current page's base URL
+		var params = parseQueryStr(window.location.search.substring(1)); // Parse the query string params into a dictionary
+		if (params['code']) { // If the query param 'code' exists, then...
+			accessCode = params['code'];
+			requestToken();
+		}
+		if (params['error']) { // If the query param 'error' exists, then something went wrong...
+			alert('Error requesting auth code: ' + params['error'] + ' / ' + params['error_description']);
+		}
 	}
 }
 
@@ -40,46 +39,47 @@ var parseQueryStr = function( queryString) {
     return params;
 };
 
+
+
 // Step #1: Requests the users Auth Code
 function requestCode() {
-	var appClientId=clientId;
 	var requestUrl = 'https://api.ciscospark.com/v1/authorize?' + //Spark OAuth2 base URL
-		'client_id=' + encodeURIComponent(appClientId) + '&' + // The custom app Client ID
+		'client_id=' + encodeURIComponent(clientId) + '&' + // The custom app Client ID
 		'response_type=code&' + // Requesting the OAuth2 'Authentication Code' flow
 		'redirect_uri=' + encodeURIComponent(appRedirectUri) + '&' + // The custom app's Redirect URI
 		'scope=spark%3Amessages_write%20spark%3Arooms_read%20spark%3Amemberships_read%20spark%3Amessages_read%20spark%3Arooms_write%20spark%3Apeople_read%20spark%3Amemberships_write&' + // Requested permission, i.e. Spark room info
 		// The following items are provided by the developer in the source code/config or generated dynamically at run time
 		'state=' + encodeURIComponent(randomString(63)) + '&' +	// Random string for OAuth2 nonce replay protection
-		'service=spark'; 
+		'service=spark';
 	window.location = requestUrl; // Redirect the browser to the OAuth2 kickoff URL
 }
 
-// Takes the auth code and requests an access token
-function requestToken() {
-	console.log("requesting token");
-	$.ajax({
-		url: "https://api.ciscospark.com/v1/access_token",
+// Step #2: Request a Token
+function requestToken(){
+		$.ajax({
+		url: "http://api.bdmcomputers.com:8080/spark/token?accessCode="+accessCode,
 		headers: {'Content-Type': 'application/json'},
 		cache: false,
-		type: "POST",
+		type: "GET",
 		dataType: 'json',
-	    data: JSON.stringify({grant_type: 'authorization_code', redirect_uri: appRedirectUri, code: accessCode, client_id: clientId, client_secret: clientSecret}),
 		statusCode: {
 			200: function(data){
+				console.log(data);
 				sparkToken = "Bearer " + data['access_token'];
+				var refresh = data['refresh_token'];
+				var refreshExp = data['exires_in'];
+				var refreshToken = data['refresh_token_expires'];
 				localStorage.setItem("sparkToken", sparkToken);
-				localStorage.setItem("refreshToken", data['refresh_token']);
-				localStorage.setItem("expires_in", data['exires_in']);
-				localStorage.setItem("refresh_token_expres", data['refresh_token_expires']);
+				localStorage.setItem("refreshToken", refresh);
+				localStorage.setItem("expires_in", refreshExp);
+				localStorage.setItem("refresh_token_expres", refreshToken);
 				me();
 			}
 		}
 	}).done(function(){
 		
 	});
-
 }
-
 
 // this function stores the users profile information in localstorage.
 function me(){

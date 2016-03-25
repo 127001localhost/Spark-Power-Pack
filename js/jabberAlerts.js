6f22aa1c4a3407701d3ada173bad8bc1d0d294a0
Brad McAllister <bdm@bdmcomputers.com>
2016-03-24 21:51:48 -0500

    IE fix.

--- js/jabberAlerts.js
+++ js/jabberAlerts.js
@@ -32,7 +32,7 @@ function createHook(){
 	        data: JSON.stringify({name: selected[i].value, targetUrl: 'http://api.bdmcomputers.com:8080/spark/'+myId, resource: 'messages', event: 'created', filter: 'roomId='+selected[i].id}),
 			statusCode: {
 				200: function(){
-					$(".container").html('<div class="jumbotron"><h2>Your Jabber Alerts have been created!</h2><p>In order to see these alerts in Jabber, follow these simple steps.<h4> Jabber for Mac & Windows:</h4> <ul><li> Mac: Open Jabber and click File -> New Custom Tab. Windows: Open Jabber and click File -> New -> Custom Tab.</li><li>"Create a new custom tab" will pop up. Provide a name for the tab (Spark Alerts). Copy and Paste the following in the page URL http://spark.bdmcomputers.com/jabberTab.php and click Create!</li><li>You will now see a new icon near the lower left of the Jabber screen. </li></ul><img src="images/tab3.jpg"></p><button class="btn btn-normal" type="button" onclick=\'window.location="powerpack.php"\'>Home</button></div>');
+					$(".container").html('<div class="jumbotron"><h2>Your Jabber Alerts have been created!</h2><p>In order to see these alerts in Jabber, follow these simple steps.<h4> Jabber for Mac & Windows:</h4> <ul><li> Mac: Open Jabber and click File -> New Custom Tab. Windows: Open Jabber and click File -> New -> Custom Tab.</li><li>"Create a new custom tab" will pop up. Provide a name for the tab (Spark Alerts). Copy and Paste the following in the page URL http://spark.bdmcomputers.com/jabberTab.php and click Create!</li><li>You will now see a new icon near the lower left of the Jabber screen. </li></ul><img src="images/tab3.jpg"></p><button class="btn btn-success" type="button" onclick=\'window.location="powerpack.php"\'>Home</button></div>');
 				}
 			}
 		});
@@ -56,7 +56,7 @@ function getHooks(){
 		var HTML= "<div class='row'><div class='col-md-6'><h2>Current Jabber Alerts</h2></div></div><div class='row'><div class='col-md-6'><table class='table table-striped' id='currentAlerts'><thead><th>Remove</th><th>Room Name</th></thead>";
 		if(data){
 			for(var alert in data['items']){
-				if(data['items'][alert].targetUrl.includes(alertUrl)){
+				if(data['items'][alert].targetUrl.indexOf(alertUrl) > -1){
 					HTML += "<tr><td><input type='checkbox' name='checkboxes' id='"+data['items'][alert].id+"' value='"+data['items'][alert].id+"'><td>"+data['items'][alert].name+"</td></tr>";
 				}
 			}
